81dfa15f8cdb668388029ccbac573bba97f41058
ZverM7 <115363819+ZverM7@users.noreply.github.com>
2022-11-22 11:40:04 +0100

    Update create_api.R

--- src/create_api.R
+++ src/create_api.R
@@ -1,3 +1,10 @@
+################################################################################################################################################
+################################################################################################################################################
+################################################################ Link Score ####################################################################
+################################################################################################################################################
+################################################################################################################################################
+
+
 # script name: Score_function
 # plumber_example.R
 
@@ -285,16 +292,19 @@ get_table <- function(foodlink){
   result <- (sum(as.numeric(ing_name$CO2_Calculated), na.rm=TRUE))
 }
 
+################################################################################################################################################
+################################################################################################################################################
+################################################################ Learn More ####################################################################
+################################################################################################################################################
+################################################################################################################################################
 
 #* Find the CO2 for the ingredient
 #* @param ingredient
-#* @param variety
-#* @param certification
 #* @get /ing_co2
 #* @response print learnmoreC02
 
 
-get_co2 <- function(ingredient, variety, certification) {
+get_co2 <- function(ingredient) {
   
   #packages to load
   library(readr)
@@ -327,14 +337,121 @@ get_co2 <- function(ingredient, variety, certification) {
   plurals_en <- data.table(dbReadTable(db, "plurals"))
   recipes_data <- data.table(dbReadTable(db, "recipes_data"))
   synonyms_data <- data.table(dbReadTable(db, "synonyms_data"))
-  
-  learnmore <- emissions_data[emissions_data[,"ingredient"]==ingredient & emissions_data[,"variety"]==variety & emissions_data[,"certification"]==certification,]
-  
+    
   learnmoreCO2 <- learnmore %>%
-    select(emissions) %>%
-    as.character()
+    filter(ingredient) %>%
   
+}
+
+################################################################################################################################################
+################################################################################################################################################
+################################################################ Manual Score ##################################################################
+################################################################################################################################################
+################################################################################################################################################
+
+# script name: Score_function
+# plumber_example.R
+
+# Define API
+
+#* @get /get_score_manual
+#* @param webtable
+#* @response print result
+
+
+webtable <- function(webtable) {
   
+  #open connection
+  ip <- "35.228.124.55"
+  db_name <- "postgres"
+  user <- "team"
+  pwd <- "pdsp22"
+
+  #Connection Setup
+  db <- DBI::dbConnect(RPostgres::Postgres(),
+                       dbname = db_name,
+                       host = ip,
+                       port = 5432,
+                       user = user,
+                       password = pwd)
+
+  #load tables
+  emissions_data <- dbReadTable(db, "emissions_data")
+  cooking_vocab_data <- dbReadTable(db, "cooking_vocab")
+  cooking_units_data <- data.table(dbReadTable(db, "cooking_units"))
+  plurals_en <- data.table(dbReadTable(db, "plurals"))
+  recipes_data <- data.table(dbReadTable(db, "recipes_data"))
+  synonyms_data <- data.table(dbReadTable(db, "synonyms_data"))
+  
+  webtable$Ingredient_parsed <- " "
+  
+  suppressWarnings({
+  #find the ingredients from the file
+  x <- 1
+  for (row in webtable$Ingredients){
+    row <- as.list(row)
+    test_ing <- amatch(row, emissions_data$ingredient, maxDist = 0.1, method = "cosine")
+    for (words in test_ing){
+      if (is.na(words) == FALSE) {
+        webtable$Ingredient_parsed[x] <- emissions_data$ingredient[words]
+      }
+      x <- x + 1
+    }}
+  
+  #add info
+  x <- 1
+  for (row in webtable$Ingredient_parsed) {
+    if (row %in% emissions_data$ingredient) {
+      #add emission
+      webtable$CO2[x] <- emissions_data %>%
+        filter(ingredient == row) %>%
+        summarize(mean(emissions)) 
+      #add variety
+      webtable$Variety[x] <- emissions_data %>%
+        filter(ingredient == row) %>%
+        select(variety)
+    } else {
+      #if nothing is parsed
+      webtable$CO2[x] <- 0
+      webtable$Variety[x] <- "N/A"
+    }
+    x <- x + 1
+  }
+  
+  cooking_units_data$KG <- c(39.37007874, 39.37007874, 1, 1, 1, 1,1,1,1000,1000,1000,1000,1,1,1,1,1,1,1,1,2,1000000,1000000, 1000000,1000000,1000000,35.27336861,35.27336861,35.27336861,2,2,1,1,1,1,1,1,4.2268,415.54,100,4.2268,4.2268,2500,10,10,10,10,10,33.8140227,2.11,1.0567,33.8140227,0.26,0.26,0.26,9.92063,26.45503,1,1,1000,1,2795.56,2795.56,2.113376419,2.113376419,2.113376419,0.01,1.1,0.001,67.6280454,67.6280454,67.6280454,67.6280454,67.6280454,202.8841362,202.8841362,202.8841362,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1000,1000,1000,1.1,2500,1,1,1,1,1, 1,117.65, 117.65, 1000, 1000, 1000, 1000, 1, 1, 1, 1, 2.20462,1,1,1,1,1,1,1,1,1,1,1, 20000,20000, 1,1, 1, 1, 1)
+  
+  #add conversion
+  x <- 1
+  for (row in webtable$Unit) {
+    if (row %in% cooking_units_data$unit) {
+      #add conversion rate
+      webtable$Conversion[x] <- cooking_units_data %>%
+        filter(unit == row) %>%
+        select(KG)
+    } else {
+      #if nothing is parsed
+      webtable$Conversion[x] <- 1
+    }
+    x <- x + 1
+  }
+  
+  x <- 1
+  for (row in webtable$Conversion){
+    webtable$CO2_Calculated[x] <- (as.numeric(webtable$CO2[x])/as.numeric(row))*as.numeric(webtable$Quantity[x])
+    x <- x + 1
+  }
+  
+  x <- 1
+  for (row in webtable$CO2_Calculated){
+    if (is.na(row) == TRUE){
+      webtable$CO2_Calculated[x] <- (as.numeric(webtable$CO2[x])/as.numeric(webtable$Conversion[x])*0.1)
+    }
+    x <- x + 1
+  }
+})
+  
+result <- (sum(as.numeric(webtable$CO2_Calculated), na.rm=TRUE))
+
 }
 
 ##
